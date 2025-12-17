6f9276ae66355c73610acd953dc4d28e6a00beac
Sandro Martínez <virgosmp.mx@gmail.com>
2025-12-17 13:25:39 -0600

    la fila de la posada

--- Las Posadas/La_fila_de_la_posada_de_Don_Liborio.cpp
+++ Las Posadas/La_fila_de_la_posada_de_Don_Liborio.cpp
@@ -1,61 +1,41 @@
-#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
 #include <bits/stdc++.h>
 
 using namespace std;
 
-using ll = long long;
-using ull = unsigned long long;
-using vi = vector<ll>;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-#define sz(x) int(x.size())
-#define fi first
-#define se second
-#define pb push_back
-#define edl '\n'
-
-constexpr long long LLINF = 2e18;
-constexpr int INF = 2e9;
-constexpr int MOD = 1e9 + 7;
-constexpr int MxN = 2e5 + 5;
-constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
-
-void solve() {
     int n;
     cin >> n;
-    queue<int> qe;
-    map<int, queue<int>> qen;
 
-    for (int i = 0; i < n; ++i) {
-        char x;
-        cin >> x;
-        if (x == 'L') {
+    queue<int> orden;                 // cola de prioridades e
+    map<int, queue<int>> grupos;      // e -> cola de ids
+
+    while (n--) {
+        char op;
+        cin >> op;
+
+        if (op == 'L') {
             int id, e;
             cin >> id >> e;
-            if (qen[e].empty()) {
-                qe.push(e);
+
+            if (grupos[e].empty()) {
+                orden.push(e);
             }
-            qen[e].push(id);
+            grupos[e].push(id);
 
-        } else if (x == 'R') {
-            int e = qe.front();
-            int n = qen[e].front();
-            cout << n << "\n";
-            qen[e].pop();
+        } else { // 'R'
+            int e = orden.front();
+            int id = grupos[e].front();
+            cout << id << '\n';
 
-            if (qen[e].empty()) {
-                qe.pop();
+            grupos[e].pop();
+            if (grupos[e].empty()) {
+                orden.pop();
             }
         }
     }
-}
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t = 1;
-    while (t--) {
-        solve();
-    }
     return 0;
 }
